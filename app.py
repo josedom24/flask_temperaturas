d7d7f5ca11f6f063e08e6c8dcac9364346542970
José Domingo Muñoz <josedom24@gmail.com>
2018-04-23 08:44:20 +0200

    Error

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flask import Flask, request,url_for,render_template,abort
 from lxml import etree
 import os
 app = Flask(__name__)	
@@ -22,4 +21,4 @@ def temperatura(code):
 
 if __name__ == '__main__':
 	port=os.environ["PORT"]
-	app.run('0.0.0.0',int(port), debug=True)
+	app.run('0.0.0.0',port, debug=True)
